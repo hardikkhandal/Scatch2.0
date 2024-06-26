7e1b9f3ab117b2857637fcd8afcdd133ad93dfec
Hardik Khandal <hardikkhandal@gmail.com>
2024-06-26 19:43:28 +0530

    commit before ruining my website

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,38 @@
 const express = require("express");
 const isLoggedIn = require("../middlewares/isLoggedIn");
 const productModel = require("../models/productModel");
+const userModel = require("../models/userModel");
 const router = express();
 
 router.get("/", function (req, res) {
   let error = req.flash("error");
-  res.render("index", { error });
+  res.render("index", { error, loggedin: false });
 });
 
 router.get("/shop", isLoggedIn, async function (req, res) {
   let products = await productModel.find();
-  res.render("shop", { products });
+  let success = req.flash("success");
+  res.render("shop", { products, success });
+});
+
+router.get("/cart", isLoggedIn, async function (req, res) {
+  let user = await userModel
+    .findOne({ email: req.user.email })
+    .populate("cart");
+
+  const finalprice =
+    Number(user.cart[0].price + 20) - Number(user.cart[0].discount);
+  // console.log(user.cart);
+
+  res.render("cart", { user, finalprice });
+});
+
+router.get("/addtocart/:productid", isLoggedIn, async function (req, res) {
+  let user = await userModel.findOne({ email: req.user.email });
+  user.cart.push(req.params.productid);
+  await user.save();
+  req.flash("success", "Product added to cart successfully");
+  res.redirect("/shop");
 });
 
 module.exports = router;
